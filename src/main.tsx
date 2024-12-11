86100348ec11c5a076401d1378532cf8669b9638
Carlo <carlo@MacBook-Pro-de-Carlo.local>
2024-12-10 23:58:10 -0500

    chore: i18n usage

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+import global_en from "./translations/en/global.json";
+import global_es from "./translations/en/global.json";
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      global: global_en
+    },
+    es: {
+      global: global_es
+    }
+  }
+})
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </StrictMode>,
+)
